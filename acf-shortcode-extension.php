10404a6673b7a2e5c85b6a769ebfce275427fb6b
Dan Schaefer <dan@schaeferzone.net>
2014-03-03 22:46:27 -0500

    Initial commit

---
+++ acf-shortcode-extension.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: ACF Shortcode Extension
+ * Plugin URI: https://github.com/lieutdan13/acf-shortcode-extension
+ * Description: An extension to the shortcode feature of the Advanced Custom Fields Wordpress plugin
+ * Version: 0.1
+ * Author: Dan Schaefer
+ * Author URI: http://www.schaeferzone.net
+ * License: WTFPL (http://www.wtfpl.net/)
+ */
+
+
---
+++ readme.txt
@@ -0,0 +1,7 @@
+=== ACF Shortcode Extension ===
+Contributors: lieutdan13
+Requires at least: 3.8.1
+License: WTFPL
+License URI: http://www.wtfpl.net/
+
+An extension to the shortcode feature of the Advanced Custom Fields Wordpress plugin

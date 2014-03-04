908c50f11e8ca45b1be4752424f616e0c6a8249a
Dan Schaefer <dan@schaeferzone.net>
2014-03-04 06:42:39 -0500

    Adding a shortcode function that returns a debug string

--- acf-shortcode-extension.php
+++ acf-shortcode-extension.php
@@ -9,4 +9,33 @@
  * License: WTFPL (http://www.wtfpl.net/)
  */
 
+/*
+*  acfx_shortcode()
+*
+*  This function is used to add advanced shortcode support for the ACF plugin
+*
+*  @type	function
+*  @since	0.1
+*  @date	03/04/2014
+*
+*  @param	array	$atts: an array holding the shortcode options
+*			string + fields: comma separated list of fields
+*			mixed  + post_id: the id of the post from which to load the fields
+*
+*  @return	string	$content: the formatted content
+*/
 
+function acfx_shortcode( $atts ) {
+	// extract attributes
+	extract( shortcode_atts( array(
+		'fields'  => '*',
+		'post_id' => false,
+	), $atts ) );
+
+	// get all field objects for this post
+	$field_objects = get_field_objects( $post_id );
+
+	$content = '<pre>' . print_r( $field_objects ) . '</pre>';
+	return $content;
+}
+add_shortcode( 'acfx', 'acfx_shortcode' );
